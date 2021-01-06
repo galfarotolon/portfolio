687eac7af11526f3911b5d329838ee058b4de944
Guillermo Alfaro <galfarotolon@gmail.com>
2021-01-06 13:31:13 -0500

    skills completed with python and also fixed navbar and arrow to scroll to correct place

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ $('.learn-more a').on('click', function (e) {
 
         const hash = this.hash;
         $('html, body').animate({
-            scrollTop: $(hash).offset().top + 10
+            scrollTop: $(hash).offset().top + 130
         }, 800)
     }
 })
